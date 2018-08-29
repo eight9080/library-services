25552047f8a3dbcaf1c3237082897e4a6baea226
eight9080 <eight9080@gmail.com>
2018-08-30 00:59:43 +0300

    book-service properties

--- book-service/src/main/java/ro/dg/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/ro/dg/bookservice/BookServiceApplication.java
@@ -2,13 +2,12 @@ package ro.dg.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookServiceApplication {
 
-	public String message;
-	public String message2;
-	public String message3;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookServiceApplication.class, args);
