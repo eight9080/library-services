a2edaeba0ef29cce92a90224a36a13aad5047886
eight9080 <eight9080@gmail.com>
2018-08-29 09:15:58 +0300

    gateway service

---
+++ gateway/src/main/java/ro/dg/gateway/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package ro.dg.gateway.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/book-service/books").permitAll()
+                .antMatchers("/eureka/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .logout().permitAll().and()
+                .csrf().disable();
+    }
+}
